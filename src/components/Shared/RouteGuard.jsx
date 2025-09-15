c52ad0f81adf3e891a332343874cdb81ee9c441b
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-09-15 18:22:15 +0300

    edit: move to Share folder

---
+++ src/components/Shared/RouteGuard.jsx
@@ -0,0 +1,20 @@
+import { Outlet, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
+export const RouteGuard = () => {
+    const navigate = useNavigate();
+
+    const token = localStorage.getItem("auth");
+
+    useEffect(() => {
+        if (!token) {
+            navigate("/login");
+        }
+
+        if (token) {
+            navigate("/user-profile")
+        }
+    },[]);
+
+    return <Outlet/>
+};
