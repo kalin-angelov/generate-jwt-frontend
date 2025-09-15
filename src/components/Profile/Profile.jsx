4124d930526c4b0c1f4f2862fe3a93b28da2e6d1
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-09-15 18:17:55 +0300

    edit: create new profile page component

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,73 @@
+import { useEffect, useContext } from "react";
+import { Link } from "react-router-dom";
+
+import { useUserStore } from "../../store/user";
+import { AuthContext } from "../../context/AuthContext";
+
+import LogoutBtn from "../Shared/LogoutBtn";
+
+const Profile = () => {
+    const { getUser } = useUserStore();
+    const { setAuth, user, setUser } = useContext(AuthContext);
+
+    useEffect(() => {
+        
+        getUser()
+            .then(data => setUser(data))
+            .catch(() => {setAuth({}), navigate("/login")})
+            
+    },[]);
+    
+    return (
+        <main>
+            <LogoutBtn />
+            <>
+                {!user ? 
+                    <div>Loading...</div> 
+                    :
+                    <>
+                        <article className="container profile-container">
+
+                            <div className="circle"></div>
+                            <h2 className="profile-header">My Pass</h2>
+
+                            <figure className="img-container">
+                                <img className="profile-img" src={user.imgUrl ? user.imgUrl : "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"} width={120} alt="profile img" />
+                            </figure>
+
+                            <h3 className="username-header">{user.username}</h3>
+
+                            <ul className="user-info">
+
+                                <li>Role : <span>{user.role}</span></li>
+                                {!user.lastName && !user.firstName 
+                                    ? 
+                                    <>
+                                        <li>First name : <span>Null</span></li>
+                                        <li>Last name : <span>Null</span></li>
+                                    </>  
+                                    :
+                                    <>
+                                        {user.firstName ? <li>First name : <span>{user.firstName}</span></li> : null}
+                                        {user.lastName ? <li>Last name : <span>{user.lastName}</span></li> : null}
+                                    </>  
+                                } 
+                                <li>Email : <span>{user.email}</span></li>
+
+                            </ul>
+
+                            <Link to={"/edit"}>
+                                <button className="btn" type="button">
+                                    Edit
+                                </button>
+                            </Link>
+                        </article>
+                    
+                    </>
+                }
+            </>
+        </main>
+    )
+};
+
+export default Profile;
